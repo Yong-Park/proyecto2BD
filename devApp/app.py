36eb11675719b395fcdea3a0cc5f3baf0ffc126d
Yong <yong.park123456@gmail.com>
2022-03-29 19:01:46 -0600

    se logra mostrar todos los valores de la base de datos

--- devApp/app.py
+++ devApp/app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+import psycopg2 #pip install psycopg2
+import psycopg2.extras
 
 app = Flask(__name__)
+app.secret_key = "cairocoders-ednalan"
 
-@app.route('/registrar')
+conn = psycopg2.connect(
+    database ="cpjklpze",
+    user = "cpjklpze",
+    password = "PbGV_JLGulr8ftaX3luPjvUZbk7q9nOI",
+    host = "raja.db.elephantsql.com",
+    port = "5432"
+)
+
+
+#pagina defaul que mostrara al correr
+@app.route('/')
 def registrar():
-    return render_template('registrar.html')
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    s = """
+    SELECT * FROM usuarios;
+    """
+    cur.execute(s)
+    list_users = cur.fetchall()
+    return render_template('index.html', list_users = list_users)
 
 if __name__ == '__main__':
     app.run(debug=True)
