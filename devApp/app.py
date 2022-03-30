9eacd7e5c1a42b3dde5a0f04b0f3fb7d48750816
Yong <yong.park123456@gmail.com>
2022-03-30 03:56:02 -0600

    ya se logra agregar, nuevos usuario y modificar

--- devApp/app.py
+++ devApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash
 import psycopg2 #pip install psycopg2
 import psycopg2.extras
 
@@ -15,15 +15,68 @@ conn = psycopg2.connect(
 
 
 #pagina defaul que mostrara al correr
-@app.route('/')
+@app.route('/', methods=['GET'])
 def registrar():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    s = """
-    SELECT * FROM usuarios;
-    """
-    cur.execute(s)
+    cur.execute(
+        """
+        SELECT * FROM usuarios;
+        """
+    )
     list_users = cur.fetchall()
     return render_template('index.html', list_users = list_users)
+#agregar usuarios a la base de datos
+@app.route('/agregar_usuario', methods=['POST'])
+def agregar_usuario():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        correo = request.form['correo']
+        cur.execute(
+            """
+            INSERT INTO usuarios (nombre, apellido, email) VALUES (%s,%s,%s)
+            """, (nombre,apellido,correo)
+        )
+        conn.commit()
+        flash('Usuario agregado exitosamente')
+        return redirect(url_for('registrar'))
+#se manda al edit.html los valores del que se selecciono
+@app.route('/edit/<id>', methods=['POST', 'GET'])
+def obtener_usuario(id):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    cur.execute(
+        """
+        SELECT * FROM usuarios WHERE id=%s
+        """,(id)
+    )
+    data = cur.fetchall()
+    cur.close()
+    print(data[0])
+    return render_template('edit.html', usuario = data[0])
+
+#al darle update del edit.html este correra y obtendra los datos y se actualizara
+@app.route('/update/<id>', methods=['POST'])
+def actualizar_usuario(id):
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        correo = request.form['email']
+
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute(
+            """
+            UPDATE usuarios
+            SET nombre = %s,
+                apellido = %s,
+                email = %s
+            WHERE id = %s
+            """,(nombre,apellido,correo, id)
+        )
+        flash('usuario actualizado exitosamente')
+        conn.commit()
+        return redirect(url_for('registrar'))
 
 if __name__ == '__main__':
     app.run(debug=True)
