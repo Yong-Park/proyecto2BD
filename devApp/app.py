8c1e136f33940a46761bac0468e820399bae130d
Rodrigo Barrera <70623939+bar20807@users.noreply.github.com>
2022-04-18 13:00:19 -0600

    Finalizado

--- devApp/app.py
+++ devApp/app.py
@@ -4,7 +4,7 @@ import psycopg2 #pip install psycopg2
 import psycopg2.extras
 import re
 import random
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash 
 
 app = Flask(__name__)
 app.secret_key = "cairocoders-ednalan"
